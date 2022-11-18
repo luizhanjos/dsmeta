920bf2c6f704ab45147dd9ee7d4ee499b81f2b6f
Luiz Henrique <luizhenrique.anjos10@hotmail.com>
2022-11-18 17:06:45 -0300

    trocando nome instagram

--- frontend/src/componentes/Header/index.tsx
+++ frontend/src/componentes/Header/index.tsx
@@ -10,7 +10,7 @@ function Header() {
                 <h1>DSMeta</h1>
                 <p>
                     Desenvolvido por
-                    <a href="https://www.instagram.com/manoloa7">@devsuperior.ig</a>
+                    <a href="https://www.instagram.com/manoloa7">@manoloa</a>
                 </p>
             </div>
         </header>
